3fc4bc16b2c5db102b3c3fcb862a95f2449a46a7
martincameron <mumart@gmail.com>
2021-03-31 22:49:28 +0100

    Dynamic quantizer for improving FLAC compression-ratio.

---
+++ wavtools/QuantizedSampleData.java
@@ -0,0 +1,90 @@
+
+package wavtools;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+
+/*
+	Dynamic-quantizer which reduces the
+	precision of high-amplitude portions of the signal.
+	Can be used with PCM-compression algorithms such as FLAC, to reduce
+	the file size by as much as half without significant loss of quality.
+*/
+public class QuantizedSampleData implements SampleData {
+	private static final String VERSION = "20210331 (c) mumart@gmail.com";
+
+	private SampleData input;
+
+	public QuantizedSampleData( SampleData sampleData ) {
+		input = sampleData;
+	}
+
+	public int getNumChannels() {
+		return input.getNumChannels();
+	}
+
+	public int getSampleRate() {
+		return input.getSampleRate();
+	}
+
+	public int getSamplesRemaining() {
+		return input.getSamplesRemaining();
+	}
+
+	public int getSamples( short[] outputBuf, int offset, int count ) throws Exception {
+		if( input.getSamplesRemaining() < count ) {
+			count = input.getSamplesRemaining();
+		}
+		int end = offset + count;
+		while( offset < end ) {
+			/* Quantize chunks of at most 64 samples. */
+			int samples = input.getSamples( outputBuf, offset, 64 );
+			for( int channel = 0, channels = input.getNumChannels(); channel < channels; channel++ ) {
+				int min = 0, max = 0;
+				for( int idx = offset, endIdx = offset + samples; idx < endIdx; idx++ ) {
+					int amp = outputBuf[ idx * channels + channel ];
+					if( amp < min ) {
+						min = amp;
+					}
+					if( amp > max ) {
+						max = amp;
+					}
+				}
+				/* Calculate the dynamic-range of this chunk. */
+				int bits = 0;
+				int variance = max - min;
+				while( variance > 0 ) {
+					bits++;
+					variance >>= 1;
+				}
+				if( bits > 7 ) {
+					/* Quantize and round. */
+					for( int idx = offset, endIdx = offset + samples; idx < endIdx; idx++ ) {
+						int amp = ( outputBuf[ idx * channels + channel ] + 32768 ) >> ( bits - 8 );
+						amp = ( amp >> 1 ) + ( amp & 1 );
+						outputBuf[ idx * channels + channel ] = ( short ) ( ( amp << ( bits - 7 ) ) - 32768 );
+					}
+				}
+			}
+			offset += samples;
+		}
+		return count;
+	}
+
+	public static void main( String[] args ) throws Exception {
+		if( args.length != 2 ) {
+			System.err.println( "Dynamic quantizer. Version " + VERSION );
+			System.err.println( "  Usage: java " + QuantizedSampleData.class.getName() + " input.wav output.wav" );
+			System.exit( 0 );
+		}
+		java.io.FileInputStream inputStream = new java.io.FileInputStream( args[ 0 ] );
+		java.io.FileOutputStream outputStream = new java.io.FileOutputStream( args[ 1 ] ); 
+		try {
+			WavSampleData.writeWav( new QuantizedSampleData( new WavSampleData( inputStream ) ), outputStream );
+		} finally {
+			outputStream.close();
+			inputStream.close();
+		}
+	}
+}
